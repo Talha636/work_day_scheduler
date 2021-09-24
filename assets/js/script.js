3b3a2de3e3f4e9eb8aac39fcab239200b769a489
Talha636 <85548877+Talha636@users.noreply.github.com>
2021-09-24 10:06:03 -0400

    html done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log('brr');
+let currentDay = $('#currentDay');
+let container = $('#container');
+
+function displayTime() {
+    let now = moment().format('dddd, MMM Do');
+    currentDay.text(now);
+}
+
+function timeBlock() {
+    for (i = 0; i < 24; i++) {
+        let row = $('<div>').addClass('row');
+        let hour = $('<div>').addClass('hour');
+        let description = $('<div>').addClass('description');
+        let save = $('<button>').addClass('saveBtn col-lg-1');
+        row.append(hour, description, save);
+        container.append(row);
+    }
+}
+timeBlock();
+displayTime();
